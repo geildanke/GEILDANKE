c748ab78b9c376d5406089cead1240f9bcb3556c
Michaela Lehr <michaela.lehr@geildanke.com>
2016-06-24 11:55:43 +0200

    GEILDANKE-354 chore (vrlive): add module, hero and live example
    - Add vrlive includes, markup, styles and scripts
    - Add geildanke.vr script

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+(function () {
+  var container = document.getElementById( 'vrlive__canvas' ),
+    threeSixtyImagePath = '/images/vr-hero.jpg';
+  new GEILDANKEVR( threeSixtyImagePath, container );
+})();
