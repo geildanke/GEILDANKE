9f2890e3f07c8b14861e33dc76b91c5803888601
Michaela Lehr <michaela.lehr@geildanke.com>
2016-09-16 16:26:23 +0200

    GEILDANKE-390 chore (content): add vr usage texts and images

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@
 (function () {
   var vrImageContainer = document.getElementById( 'vrlive__image__canvas' ),
     vrVideoContainer = document.getElementById( 'vrlive__video__canvas' ),
-    vrImagePath = '/images/vr-map.jpg',
+    vrImagePath = '/images/vr-italy.jpg',
     vrVideoPath = '/images/videos/R0010084_20160628124206_20160903212208.MP4',
     vrImage = new GEILDANKEVR( 'image', vrImagePath, vrImageContainer ),
     vrVideo = new GEILDANKEVR( 'video', vrVideoPath, vrVideoContainer );
