da6f06d9004e7ca08eb3b037efd163c31c2beaf5
Michaela Lehr <michaela.lehr@geildanke.com>
2016-10-25 14:50:19 +0200

    GEILDANKE-412 feat (hero): use new fullscreen image

--- scripts/main.js
+++ scripts/main.js
@@ -5,21 +5,10 @@
     container: document.getElementById('vrlive__image__canvas'),
     single: {
       mediaType: 'image',
-      url: '/images/vr-italy.jpg',
+      url: '/images/vr_mountain.jpg',
       optionalConfig: {
         startRotation: 70,
-        isAnimation: false
-      }
-    }
-  });
-  GEILDANKEVR.single({
-    container: document.getElementById('vrlive__video__canvas'),
-    single: {
-      mediaType: 'video',
-      url: '/images/videos/R0010084_20160628124206_20160903212208.MP4',
-      optionalConfig: {
-        startRotation: 90,
-        isAnimation: false
+        isAnimation: true
       }
     }
   });
