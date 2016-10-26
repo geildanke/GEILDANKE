b48aa9d7a5b304126b2b5c2c617ced139e30ad7e
Michaela Lehr <michaela.lehr@geildanke.com>
2016-10-26 16:34:39 +0200

    GEILDANKE-412 feat (main.js): show and hide buttons, new image

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,87 @@
 'use strict';
 
 (function () {
-  GEILDANKEVR.single({
-    container: document.getElementById('vrlive__image__canvas'),
-    single: {
-      mediaType: 'image',
-      url: '/images/vr_mountain.jpg',
-      optionalConfig: {
-        startRotation: 70,
-        isAnimation: true
+  var activateHeroVr = {},
+    activateExampleVrImage = {},
+    activateExampleVrVideo = {},
+    heroButton = {},
+    heroContainer = {},
+    liveExampleButtonImage = {},
+    liveExampleButtonVideo = {},
+    liveExampleContainer = {},
+    removeVr = {};
+
+  heroButton = document.getElementById('hero_vr_start');
+  heroContainer = document.getElementById('vrlive__image__canvas');
+  liveExampleContainer = document.getElementById('vrlive__video__canvas');
+  liveExampleButtonImage = document.getElementById('vrlive_vrimage_start');
+  liveExampleButtonVideo = document.getElementById('vrlive_vrvideo_start');
+
+  activateHeroVr = function () {
+    GEILDANKEVR.single({
+      container: heroContainer,
+      single: {
+        mediaType: 'image',
+        url: '/images/vr-mountain.jpg',
+        optionalConfig: {
+          startRotation: 70,
+          isAnimation: true
+        }
       }
-    }
+    });
+  };
+
+  activateExampleVrImage = function () {
+    GEILDANKEVR.single({
+      container: liveExampleContainer,
+      single: {
+        mediaType: 'image',
+        url: '/images/vr-mountain.jpg',
+        optionalConfig: {
+          startRotation: 70,
+          isAnimation: true
+        }
+      }
+    });
+  };
+
+  activateExampleVrVideo = function () {
+    GEILDANKEVR.single({
+      container: liveExampleContainer,
+      single: {
+        mediaType: 'video',
+        url: '/images/videos/R0010084_20160628124206_20160903212208.MP4',
+        optionalConfig: {
+          startRotation: 70,
+          isAnimation: true
+        }
+      }
+    });
+  };
+
+  removeVr = function (container, placeholderImageUrl, button) {
+    container.innerHTML = '';
+    container.style.backgroundImage = 'url(' + placeholderImageUrl + ')';
+    container.style.backgroundSize = 'cover';
+    container.style.backgroundPosition = 'center center';
+    button.style.display = 'block';
+  };
+
+  liveExampleButtonImage.addEventListener('click', function () {
+    removeVr(heroContainer, '/images/michaela.jpg', heroButton);
+    removeVr(liveExampleContainer, '/images/michaela.jpg', liveExampleButtonImage);
+    activateExampleVrImage();
   });
+
+  liveExampleButtonVideo.addEventListener('click', function () {
+    removeVr(heroContainer, '/images/michaela.jpg', heroButton);
+    removeVr(liveExampleContainer, '/images/michaela.jpg', liveExampleButtonVideo);
+    activateExampleVrVideo();
+  });
+
+  heroButton.addEventListener('click', function () {
+    activateHeroVr();
+  });
+
+  activateHeroVr();
 })();
