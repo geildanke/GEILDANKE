9c6de1c90ddeaff52bdc3a27b349eb7f2d24ffcc
Michaela Lehr <michaela.lehr@geildanke.com>
2016-10-28 14:34:03 +0200

    GEILDANKE-412 doc (main.js): add jsdocs

--- scripts/main.js
+++ scripts/main.js
@@ -25,6 +25,14 @@
   liveExampleButtonImage = document.getElementById('vrlive_vrimage_start');
   liveExampleButtonVideo = document.getElementById('vrlive_vrvideo_start');
 
+  /**
+   * Activate VR Plugin on Hero Placeholder
+   * - remove all VR Plugins
+   * - hide the start button
+   * - activate VR Plugin on Hero
+   *
+   * @return { void }
+   */
   activateHeroVr = function () {
     removeAllVr();
     heroButton.style.visibility = 'hidden';
@@ -41,6 +49,14 @@
     });
   };
 
+  /**
+   * Activate VR Plugin on Live Image Example
+   * - remove all VR Plugins
+   * - hide the start button
+   * - activate VR Plugin on Live Image Example
+   *
+   * @return { void }
+   */
   activateExampleVrImage = function () {
     removeAllVr();
     liveExampleButtonImage.style.visibility = 'hidden';
@@ -57,6 +73,14 @@
     });
   };
 
+  /**
+   * Activate VR Plugin on Live Video Example
+   * - remove all VR Plugins
+   * - hide the start button
+   * - activate VR Plugin on Live Video Example
+   *
+   * @return { void }
+   */
   activateExampleVrVideo = function () {
     removeAllVr();
     liveExampleButtonVideo.style.visibility = 'hidden';
@@ -73,23 +97,46 @@
     });
   };
 
+  /**
+   * Add Event Listeners
+   *
+   * @return { void }
+   */
   addListeners = function () {
     liveExampleButtonImage.addEventListener('click', activateExampleVrImage);
     liveExampleButtonVideo.addEventListener('click', activateExampleVrVideo);
     heroButton.addEventListener('click', activateHeroVr);
   };
 
+  /**
+   * Initialize Page
+   *
+   * @return { void }
+   */
   init = function () {
     addListeners();
     activateHeroVr();
   };
 
+  /**
+   * Remove All VR Plugins
+   *
+   * @return { void }
+   */
   removeAllVr = function () {
     removeVr(heroContainer, '/images/vr-mountain-placeholder.jpg', heroButton);
     removeVr(liveExampleContainerVideo, '', liveExampleButtonVideo);
     removeVr(liveExampleContainerImage, '', liveExampleButtonImage);
   };
 
+  /**
+   * Remove a special VR Plugin
+   *
+   * @param  { object } container           container element
+   * @param  { object } placeholderImageUrl placeholder image path
+   * @param  { object } button              button element
+   * @return { void }
+   */
   removeVr = function (container, placeholderImageUrl, button) {
     GEILDANKEVR.remove();
     container.innerHTML = '';
